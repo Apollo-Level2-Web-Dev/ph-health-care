faf6058446d2ad9f722f7ee8277552bdffa3d918
Shafayathub <shafayat.ms@gmail.com>
2025-11-02 04:53:01 +0600

    part-3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@/Providers/UserProvider";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,6 +17,7 @@ export const metadata: Metadata = {
   description: "A healthcare application built with Next.js",
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <UserProvider>
-        {children}
+            {children}
         </UserProvider>
       </body>
     </html>
